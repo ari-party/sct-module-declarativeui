2dc97d0bd88ed547c17c7ad792440b281fe166d4
astrid <git@astrid.email>
2024-02-22 06:06:51 +0100

    match wiki spec

--- src/spec.lua
+++ src/spec.lua
@@ -1,4 +1,17 @@
-local createElement = require( 'createElement' )
+local createElement = require( 'Module:DeclarativeUI' )
 
-print( createElement( 'div', nil ):render() )
-print( createElement( 'div', { style = 'display: none' }, 'Hello World' ):render() )
+-- You can create a simple element like this
+createElement( 'div' ):render() -- <div/>
+
+-- Add some attributes
+createElement( 'div', { a = 'b' } ):render() -- <div a="b"/>
+
+-- What about some text?
+createElement( 'h1', nil, 'Scream!' ):render() -- <h1>Scream!</h1>
+
+-- What about other elements?
+local someElement = createElement( 'span', { style = 'color: red' }, 'Hello World' )
+createElement( 'div', nil, 'This goes first', someElement ) -- <div>This goes first<span style="color: red">Hello World</span></div>
+
+-- You can have as much children as you want
+createElement( 'div', nil, 'a', 'b', 'c' ) -- <div>abc</div>
