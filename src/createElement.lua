afdca7ef05fa8011193e4dc18170c90bce0ced4e
astrid <git@astrid.email>
2024-02-22 05:45:18 +0100

    allow element arrays for children

--- src/createElement.lua
+++ src/createElement.lua
@@ -2,15 +2,36 @@ local render = require( 'render' )
 
 ---@alias Element { component: string; attributes: table<string, any>; children: table, render?: function }
 
+local function iterateChildren( child )
+    local children = {}
+
+    if type( child ) == 'table' and child[ 1 ] then
+        for _, value in ipairs( iterateChildren( child ) ) do
+            table.insert( children, value )
+        end
+    else
+        table.insert( children, child )
+    end
+
+    return children
+end
+
 ---@param component string HTML Tag
 ---@param attributes table<string, any>? HTML Attributes
----@param ... Element | string
+---@param ... Element | Element[] | string
 ---@return Element
 return function ( component, attributes, ... )
+    local children = {}
+    for _, child in ipairs( { ... } ) do
+        for _, iteratedChild in ipairs( iterateChildren( child ) ) do
+            table.insert( children, iteratedChild )
+        end
+    end
+
     local element = {
         component = component,
         attributes = attributes or {},
-        children = { ... }
+        children = children
     }
 
     function element:render()
